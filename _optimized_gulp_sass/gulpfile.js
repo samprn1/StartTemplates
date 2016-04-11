62c781c25e918c9e9a3a31a1105c797ac5fd9858
samprn1 <samprn1@gmail.com>
2016-04-11 17:42:40 +0300

    1st

---
+++ _optimized_gulp_sass/gulpfile.js
@@ -0,0 +1,35 @@
+var gulp         = require('gulp'),
+		sass         = require('gulp-sass'),
+		autoprefixer = require('gulp-autoprefixer'),
+		minifycss    = require('gulp-minify-css'),
+		rename       = require('gulp-rename'),
+		browserSync  = require('browser-sync').create();
+
+gulp.task('browser-sync', ['styles'], function() {
+		browserSync.init({
+				server: {
+						baseDir: "./app"
+				},
+				notify: false
+		});
+});
+
+gulp.task('styles', function () {
+	return gulp.src('sass/*.sass')
+	.pipe(sass({
+		includePaths: require('node-bourbon').includePaths
+	}).on('error', sass.logError))
+	.pipe(rename({suffix: '.min', prefix : ''}))
+	.pipe(autoprefixer({browsers: ['last 15 versions'], cascade: false}))
+	.pipe(minifycss())
+	.pipe(gulp.dest('app/css'))
+	.pipe(browserSync.stream());
+});
+
+gulp.task('watch', function () {
+	gulp.watch('sass/*.sass', ['styles']);
+	gulp.watch('app/js/*.js').on("change", browserSync.reload);
+	gulp.watch('app/*.html').on('change', browserSync.reload);
+});
+
+gulp.task('default', ['browser-sync', 'watch']);
